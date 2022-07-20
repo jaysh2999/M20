717ec500688b889f43330e2856a3a78cb5fdbd24
mayur <mayur@LAPTOP-FLGR9891>
2022-07-20 18:58:51 +0530

    first commit

---
+++ DAy2/src/simple.java
@@ -0,0 +1,13 @@
+
+public class simple {
+
+	public static void main(String[] args) {
+int ba=12;
+int ht=24;
+float area=0.0f;
+area=ba*ht;
+System.out.println("Area of triangle="+area);
+
+	}
+
+}
