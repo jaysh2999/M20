d31a2d0443461f9ac19f9758357d6c09a1f3503b
mayur <mayur@LAPTOP-FLGR9891>
2022-07-27 14:41:55 +0530

    fourth

---
+++ 8Day/src/com/tns/java/ArrayListDemo.java
@@ -0,0 +1,44 @@
+package com.tns.java;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo<T> {
+
+	public static void main(String []args) {
+		ArrayListDemo<Integer> t1=new ArrayListDemo<Integer>();
+		
+		
+		ArrayListDemo<String> t2=new ArrayListDemo<String>();
+		
+		
+		/*//Array
+		  String[] A=new String[500];
+		  A[0]="Avantika";
+		  A[1]="Rangini";
+		  A[2]=45;
+		 */
+		
+//	          ArrayList<String>  s=new ArrayList<String>();
+//             s.add("Avantika");
+//             s.add("Damini");
+//             s.add(45);
+		
+//             String employee1= (String)s.get(0);
+		
+//             String employee2= (String)s.get(1);
+		
+//             String employee3= (String)s.get(2);
+		
+//	    ArrayList l=new ArrayList(); //Growable in nature
+//      l.add("A");
+//      l.add(1000);
+//      l.add(4.5);
+//		l.add(null);
+//		l.remove(2);
+		
+//	    l.add(2,"M");
+//	    l.add("N");
+//      System.out.println(l);
+		
+	}
+}
