f5bdfecf254ebaadf395a195de5d959060df03b6
jaysh2999 <92245778+jaysh2999@users.noreply.github.com>
2022-08-18 17:29:26 +0530

    Add files via upload

---
+++ 11day/src/com/tns/java/TreeSetDemo.java
@@ -0,0 +1,41 @@
+//Demo on Tree Set
+package com.tns.java;
+
+import java.util.*;
+
+class TreeSetDemo {
+	public static void main(String args[]) 
+	
+	{	TreeSet treeSet1=new TreeSet();
+		treeSet1.add(56);
+		treeSet1.add(62);
+		treeSet1.add(50);
+		treeSet1.add(10);
+		treeSet1.add(15);
+		treeSet1.add("62");
+		
+	
+		
+		//Ascending order
+		System.out.println(treeSet1);
+	
+		// remove()
+		// clone()
+	
+//		TreeSet<String> treeSet2 = new TreeSet<String>(); 
+//
+//		treeSet2.add("One");
+//		treeSet2.add("Two");
+//		treeSet2.add("Three");
+//		treeSet2.add("Four");
+//		treeSet2.add("Five");
+//
+//		System.out.println("Contents of treeset");
+//		//cursor 
+//		Iterator iterator = treeSet2.iterator(); // obtaining iterator object
+//		while (iterator.hasNext()) { // to iterate through collection.
+//			Object object = iterator.next();
+//			System.out.print(object + "\t");
+//		}
+	}
+}
